ebc0442e3273e4f8f3007bb92796d2e7b11a1038
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-11-13 23:21:03 +0200

    update Register html

--- client/src/containers/register/Register.jsx
+++ client/src/containers/register/Register.jsx
@@ -24,35 +24,39 @@ export default function Register() {
         function handleChange(event) {
             setValue(event.target.value);
             validate(event);
-        };
+        }
 
         function validate(event) {
             const name = event.target.id;
 
-            schema.fields[name].validate(event.target.value, { abortEarly: false })
+            schema.fields[name]
+                .validate(event.target.value, { abortEarly: false })
                 .then(() => {
                     setErrors({ ...errors, [name]: [] });
                 })
                 .catch((err) => {
                     setErrors({ ...errors, [name]: err.errors });
                 });
-        };
+        }
 
         return { value, onChange: handleChange };
-    };
+    }
 
-    function handleSubmit() {
+    function handleSubmit(e) {
+        e.preventDefault();
+
         const data = {
             username: username.value,
             email: email.value,
             password: password.value,
             repeatPassword: repeatPassword.value,
-            subscribe: subscribe
+            subscribe: subscribe,
         };
-        const hasErrors = Object.keys(errors).filter(key => errors[key].length > 0);
+        console.log(data);
+        const hasErrors = Object.keys(errors).filter((key) => errors[key].length > 0);
 
         if (data.password !== data.repeatPassword) {
-            return setErrors({ ...errors, repeatPassword: ['Passwords doesn\'t match'] });
+            return setErrors({ ...errors, repeatPassword: ["Passwords doesn't match"] });
         }
 
         if (hasErrors.length === 0 && data.username && data.email && data.password) {
@@ -69,49 +73,63 @@ export default function Register() {
                     });
                 });
         }
-    };
+    }
 
     return (
-        <div className="main-container">
-            <form>
-                <h1>Register</h1>
-                <p>Please fill this form to create your Account.</p>
+        <div className="form-wrapper register">
+            <form onSubmit={handleSubmit}>
+                <header className="form-header">
+                    <h1>Register</h1>
+                    <p>Please fill this form to create your Account.</p>
+                </header>
 
-                <div className="form-div-container">
-                    <label htmlFor="username"><b>Username:</b></label>
+                <div className="form-field">
+                    <label htmlFor="username">Username:</label>
                     <input type="text" placeholder="Enter your Username" className="form-input" id="username" {...username} />
                     {errors.username && <div className="form-input-error">{errors.username[0]}</div>}
                 </div>
 
-                <div className="form-div-container">
-                    <label htmlFor="email"><b>Email:</b></label>
+                <div className="form-field">
+                    <label htmlFor="email">Email:</label>
                     <input type="text" placeholder="Enter your Email" className="form-input" id="email" {...email} />
                     {errors.email && <div className="form-input-error">{errors.email[0]}</div>}
                 </div>
 
-                <div className="form-div-container">
-                    <label htmlFor="password"><b>Password:</b></label>
+                <div className="form-field">
+                    <label htmlFor="password">Password:</label>
                     <input type="password" placeholder="Enter your Password" className="form-input" id="password" {...password} />
                     {errors.password && <div className="form-input-error">{errors.password[0]}</div>}
                 </div>
 
-                <div className="form-div-container">
-                    <label htmlFor="repeat-password"><b>Repeat-Password:</b></label>
+                <div className="form-field">
+                    <label htmlFor="repeat-password">Repeat-Password:</label>
                     <input type="password" placeholder="Confirm your Password" className="form-input" id="repeatPassword" {...repeatPassword} />
                     {errors.repeatPassword && <div className="form-input-error">{errors.repeatPassword[0]}</div>}
                 </div>
 
-                <div>
-                    <p>By creating an account, you agree to our <button className="info-button" onClick={handleRoute('/terms-and-conditions')}>Terms & Conditions</button>.</p>
-                    <p className="register-checkbox">I do accept to receive feedback on my email:<input type="checkbox" onClick={(e) => setSubscribe(e.target.checked)} /><b>Subscribe</b>.</p>
+                <div className="form-field">
+                    <p>By creating an account, you agree to our</p>
+                    <button className="info-button" onClick={handleRoute('/terms-and-conditions')}>
+                        Terms &amp; Conditions
+                    </button>
+
+                    <p className="register-checkbox">
+                        I do accept to receive feedback on my email:
+                        <input type="checkbox" onClick={(e) => setSubscribe(e.target.checked)} />
+                        Subscribe.
+                    </p>
                 </div>
 
-                <div>
-                    <button type="button" className="form-action-btn" onClick={handleSubmit}>Register</button>
+                <div className="form-field">
+                    <input type="submit" className="form-action-btn" value="register" />
+                    <input type="reset" className="form-action-btn" value="reset" />
                 </div>
 
                 <div className="info-container">
-                    <p>Already have account? <button className="info-button" onClick={handleRoute('/login')}>Sign in</button>.</p>
+                    <p>Already have account? .</p>
+                    <button className="info-button" onClick={handleRoute('/login')}>
+                        Sign in
+                    </button>
                 </div>
             </form>
         </div>
