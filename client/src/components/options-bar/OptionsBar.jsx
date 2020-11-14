262c2ea9ebb6b6711dd29b7d627d565e5cbdd9f2
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-11-15 01:45:58 +0200

    updating OptionBar html and styles

--- client/src/components/options-bar/OptionsBar.jsx
+++ client/src/components/options-bar/OptionsBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import history from 'utils/history';
-import './option-bar.css';
 
 function Options(props) {
     const isLogged = props.isLogin;
@@ -11,14 +10,32 @@ function Options(props) {
         history.push(name);
     };
 
-    return (isLogged ?
-        <div className="options-bar" >
-            <button className="options-bar-button" onClick={handleRoute('/game/progress')}>Progress</button>
-            <button className="options-bar-button" onClick={handleRoute('/game/shop')}>Shop</button>
-            <button className="options-bar-button" onClick={handleRoute('/game/inventory')}>Inventory</button>
-            <button className="options-bar-button" onClick={handleRoute('/game/character')}>Character</button>
-        </div >
-        : null);
+    return isLogged ? (
+        <div className="options-bar">
+            <ul>
+                <li>
+                    <a className="options-bar-button" onClick={handleRoute('/game/progress')}>
+                        Progress
+                    </a>
+                </li>
+                <li>
+                    <a className="options-bar-button" onClick={handleRoute('/game/shop')}>
+                        Shop
+                    </a>
+                </li>
+                <li>
+                    <a className="options-bar-button" onClick={handleRoute('/game/inventory')}>
+                        Inventory
+                    </a>
+                </li>
+                <li>
+                    <a className="options-bar-button" onClick={handleRoute('/game/character')}>
+                        Character
+                    </a>
+                </li>
+            </ul>
+        </div>
+    ) : null;
 }
 
 function mapStateToProps(state) {
