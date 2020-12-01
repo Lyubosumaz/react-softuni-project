ba09042eaa8053a0c8c49f0e0b665f54675087d3
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-12-01 23:18:42 +0200

    quick save

--- client/src/utils/toastHandler.js
+++ client/src/utils/toastHandler.js
@@ -1,4 +1,5 @@
 import { toast } from 'react-toastify';
+import { css } from 'glamor';
 
 toast.configure({
     autoClose: 6000,
@@ -6,9 +7,34 @@ toast.configure({
     position: toast.POSITION.TOP_RIGHT,
 });
 
+const customToast = {
+    success(msg, options = {}) {
+        return toast.success(msg, {
+            className: {
+                borderRadius: '10px',
+            },
+            ...options,
+        });
+    },
+};
+
 function toastSuccess(res) {
+    // toast(res.message ? res.message : res, {
+    //     type: toast.TYPE.SUCCESS,
+    // });
+
+    console.log('here');
+    // customToast.success('+++++++++++++++++');
     toast(res.message ? res.message : res, {
-        type: toast.TYPE.SUCCESS,
+        className: css({
+            background: 'black',
+        }),
+        bodyClassName: css({
+            fontSize: '60px',
+        }),
+        progressClassName: css({
+            background: 'repeating-radial-gradient(circle at center, red 0, blue, green 30px)',
+        }),
     });
 }
 
