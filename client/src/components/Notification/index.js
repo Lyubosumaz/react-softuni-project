5e7fef5ea573cba6b666a3d2b5a6a76a4351bb3a
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-12-02 21:56:19 +0200

    quick save

--- client/src/components/Notification/index.js
+++ client/src/components/Notification/index.js
@@ -1,7 +1,39 @@
+import { useEffect, useState, useRef } from 'react';
+
 export default function Notification() {
+    const testText = 'sadsadadsasdasdasda dasd a d adasdasdasd das dasdd!';
+    const testTime = 2;
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        document.documentElement.style.setProperty('--notification-animation-duration', `${testTime}s`);
+        autoClose();
+    }, []);
+
+    const renders = useRef(0);
+    console.log('Notification times was rendered: ', renders.current++);
+
+    function autoClose() {
+        let interval = setTimeout(() => {
+            // setVisible(false);
+            console.log('AutoClose the Notification');
+        }, testTime * 1000);
+
+        return () => clearTimeout(interval);
+    }
+
+    function handlerClose() {
+        console.log('here');
+        setVisible(false);
+    }
+
     return (
-        <div className="notification">
-            <span>Notification</span>
-        </div>
+        <section className={`notification ${visible ? 'display' : 'hidden'}`}>
+            <p>{testText}</p>
+            <button onClick={handlerClose}>Close</button>
+            <div className="progress-bar">
+                <span className="bar"></span>
+            </div>
+        </section>
     );
 }
