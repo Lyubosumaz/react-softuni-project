81a3819defeff541580b20c26cc31f6cb776d8c3
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-12-08 23:59:51 +0200

    update props delete meme container

--- client/src/containers/social/delete-meme/index.js
+++ client/src/containers/social/delete-meme/index.js
@@ -1,14 +1,21 @@
+import { PropTypes } from 'prop-types';
 import Title from '../../../components/Title';
 import DeleteMeme from '../../../components/Social/DeleteMeme';
 import Info from '../../../components/Info';
 import { containerWrapper } from '../../../class-names.json';
 
-export default function DeleteMemeContainer(props) {
+export default function DeleteMemeContainer({ match }) {
+    const memeId = match.params.id;
+
     return (
         <section className={`${containerWrapper}`}>
             <Title />
-            <DeleteMeme memeId={props.match.params.id} />
+            <DeleteMeme memeId={memeId} />
             <Info />
         </section>
     );
 }
+
+DeleteMemeContainer.propTypes = {
+    match: PropTypes.object.isRequired,
+};
