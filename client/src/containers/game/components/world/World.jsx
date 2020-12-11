01149f0763e8a88cf156f4fe4193e291e2bd6834
Lyubosumaz <lyuboslavilkov@gmail.com>
2020-12-11 18:38:17 +0200

    update props World component

--- client/src/containers/game/components/world/World.jsx
+++ client/src/containers/game/components/world/World.jsx
@@ -1,12 +1,11 @@
 import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 import Map from '../map/Map';
 import Player from '../player/Player';
-
 import { tiles } from '../data/maps/1';
 import { store } from '../../../../services/store';
-// import { setTiles } from '../map/actions';
 
-function World(props) {
+function World({ setTiles }) {
     //TODO level system
     // switch (props.level) {
     //     case 1:
@@ -23,7 +22,7 @@ function World(props) {
         type: 'ADD_TILES',
         payload: {
             tiles,
-        }
+        },
     });
 
     return (
@@ -32,8 +31,9 @@ function World(props) {
                 position: 'relative',
                 width: '1650px',
                 height: '650px',
-                margin: '20px auto'
-            }}>
+                margin: '20px auto',
+            }}
+        >
             <Map />
             <Player />
         </div>
@@ -48,13 +48,18 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        setTiles: (data) => dispatch({
-            type: 'ADD_TILES',
-            payload: {
-                data,
-            }
-        }),
+        setTiles: (data) =>
+            dispatch({
+                type: 'ADD_TILES',
+                payload: {
+                    data,
+                },
+            }),
     };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(World);
+
+World.propTypes = {
+    setTiles: PropTypes.func.isRequired,
+};
