91d8e0191aa3a09b3070e714fab1a4483656b0f6
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-05 19:58:52 +0200

    update Footer component buttons

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,21 +1,26 @@
+import { useState } from 'react';
 import Button from '../Button';
+import { factoryButtons } from '../../utils/factory';
 
 export default function Footer() {
+    const [activated, setActivated] = useState();
+
+    function handleCallBack(buttonDirection) {
+        setActivated(buttonDirection);
+    }
+
+    const navAttributes = { activated, handleCallBack, buttonStyles: 'footer-nav-button' };
+    const initializedNavBtn = factoryButtons(navAttributes);
+
     return (
         <footer className="site-footer">
             <div className="site-wrapper">
                 <p className="copy-write">Lyubosumaz SoftUni Final Project. All rights reserved &copy; 2019-2021</p>
 
                 <ul className="footer-nav">
-                    <li>
-                        <Button additionalClassName="footer-nav-button" buttonText="Home" direction="home" />
-                    </li>
-                    <li>
-                        <Button additionalClassName="footer-nav-button" buttonText="About" direction="about" />
-                    </li>
-                    <li>
-                        <Button additionalClassName="footer-nav-button" buttonText="Terms" direction="terms-and-conditions" />
-                    </li>
+                    <li>{initializedNavBtn('home')}</li>
+                    <li>{initializedNavBtn('about')}</li>
+                    <li>{initializedNavBtn('terms-and-conditions', 'Terms')}</li>
                 </ul>
             </div>
         </footer>
