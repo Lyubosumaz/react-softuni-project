993add9ee59a8ebbdcb980d8b724e4a1ebf9f1c0
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-05 22:38:52 +0200

    update Options component

--- client/src/components/OptionBar/index.js
+++ client/src/components/OptionBar/index.js
@@ -1,25 +1,29 @@
+import { useState } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
-import Button from '../Button';
+import { factoryButtons } from '../../utils/factory';
 
+// TODO rename this component to GameOptionBar
+// TODO need to fix the Router, atm it makes re-rendering
+// TODO make this component reusable for many games
 function Options({ isLogin }) {
     const isLogged = isLogin;
+    const [activated, setActivated] = useState();
+
+    function handleCallBack(buttonDirection) {
+        setActivated(buttonDirection);
+    }
+
+    const optionsAttributes = { activated, handleCallBack, buttonStyles: 'options-bar-button' };
+    const initializedOptionsBtn = factoryButtons(optionsAttributes);
 
     return isLogged ? (
         <div className="options-bar">
             <ul>
-                <li>
-                    <Button additionalClassName="options-bar-button" buttonText="Progress" direction="game/progress" />
-                </li>
-                <li>
-                    <Button additionalClassName="options-bar-button" buttonText="Shop" direction="game/shop" />
-                </li>
-                <li>
-                    <Button additionalClassName="options-bar-button" buttonText="Inventory" direction="game/inventory" />
-                </li>
-                <li>
-                    <Button additionalClassName="options-bar-button" buttonText="Character" direction="game/character" />
-                </li>
+                <li>{initializedOptionsBtn('game/progress', 'Progress')}</li>
+                <li>{initializedOptionsBtn('game/shop', 'Shop')}</li>
+                <li>{initializedOptionsBtn('game/inventory', 'Inventory')}</li>
+                <li>{initializedOptionsBtn('game/character', 'Character')}</li>
             </ul>
         </div>
     ) : null;
