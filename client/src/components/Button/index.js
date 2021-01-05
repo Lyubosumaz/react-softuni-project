6ba313860c4508d005aade80cd7e820f45cc1199
Lyubosumaz <lyuboslavilkov@gmail.com>
2021-01-05 19:23:18 +0200

    update Button component and adding props types

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,22 +1,28 @@
+import { PropTypes } from 'prop-types';
 import { handleRoute } from '../../utils/history';
 
-export default function Button({ buttonClass, buttonText, direction, myFunc, callbackDirection, isClicked }) {
-    const options = direction
-        ? {
-              onClick: (event) => {
-                  event.preventDefault();
+export default function Button({ direction, buttonClass, buttonText, callbackDirection, isClicked, myFunc }) {
+    const handleClick = direction
+        ? (event) => {
+              event.preventDefault();
 
-                  handleRoute(`/${direction}`);
-                  if (callbackDirection) callbackDirection(direction);
-              },
+              handleRoute(`/${direction}`);
+              if (callbackDirection) callbackDirection(direction);
           }
-        : {
-              onClick: { myFunc },
-          };
+        : myFunc;
 
     return (
-        <button className={`${buttonClass} ${isClicked ? 'isClicked' : ''}`} {...options}>
+        <button className={`${buttonClass} ${isClicked ? 'isClicked' : ''}`} onClick={handleClick}>
             <span>{buttonText}</span>
         </button>
     );
 }
+
+Button.propTypes = {
+    direction: PropTypes.string,
+    buttonText: PropTypes.string.isRequired,
+    buttonClass: PropTypes.string,
+    callbackDirection: PropTypes.func,
+    isClicked: PropTypes.bool,
+    myFunc: PropTypes.func,
+};
